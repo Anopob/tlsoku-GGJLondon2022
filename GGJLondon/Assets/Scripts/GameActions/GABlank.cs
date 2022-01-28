60f756d6d9dcdef739464fbd5e1a9acd677a554d
Anopob <anopob@hotmail.com>
2022-01-28 18:32:18 -0500

    Add GABlank

---
+++ GGJLondon/Assets/Scripts/GameActions/GABlank.cs
@@ -0,0 +1,12 @@
+﻿public class GABlank : GameAction
+{
+    public GABlank(int x, int y, Board board) : base(x, y, board) { }
+
+    public override void Redo()
+    {
+    }
+
+    public override void Undo()
+    {
+    }
+}
