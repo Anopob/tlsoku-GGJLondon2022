0acc1e24fd50ca7aa1eb315b92b3a6150d60cf4e
Anopob <anopob@hotmail.com>
2022-01-28 17:14:05 -0500

    Fix GameActions namespace

--- GGJLondon/Assets/Scripts/GameActions/GARightSwap.cs
+++ GGJLondon/Assets/Scripts/GameActions/GARightSwap.cs
@@ -5,22 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
-namespace Assets.Scripts.Actions
+public class GARightSwap : GameAction
 {
-    public class GARightSwap : GameAction
+    public GARightSwap(int x, int y, Board board) : base(x, y, board)
     {
-        public GARightSwap(int x, int y, Board board) : base (x, y, board)
-        {
-        }
+    }
 
-        public override void Redo()
-        {
-            _board.SwapTiles(_x, _y, _x+1, _y);
-        }
+    public override void Redo()
+    {
+        _board.SwapTiles(_x, _y, _x + 1, _y);
+    }
 
-        public override void Undo()
-        {
-            _board.SwapTiles(_x, _y, _x+1, _y);
-        }
+    public override void Undo()
+    {
+        _board.SwapTiles(_x, _y, _x + 1, _y);
     }
 }
