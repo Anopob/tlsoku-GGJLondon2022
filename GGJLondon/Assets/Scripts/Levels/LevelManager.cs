6875e10450843e29d95ad6368c9570c2a9611132
Neil Warnock <Neil@ecere.com>
2022-01-28 17:53:06 -0500

    Added level manager to scene

--- GGJLondon/Assets/Scripts/Levels/LevelManager.cs
+++ GGJLondon/Assets/Scripts/Levels/LevelManager.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public abstract class LevelManager : MonoBehaviour
 {
-    protected Board _board1, _board2;
-    protected Stack<GameAction> _gameActions;
+    [SerializeField] protected Board _board1, _board2;
+    protected Stack<GameAction> _gameActions = new Stack<GameAction>();
 
     public void OnUndoClick()
     {
@@ -20,15 +20,16 @@ public abstract class LevelManager : MonoBehaviour
     public abstract void OnLeftClick(int x, int y);
     public abstract void OnRightClick(int x, int y);
     
-    
     // EVENTS
     public void TileLeftClick(Vector2Int data)
     {
+        Debug.Log("Manager sees left click");
         OnLeftClick(data[0], data[1]);   
     }
     
     public void TileRightClick(Vector2Int data)
     {
+        Debug.Log("Manager sees right click");
         OnRightClick(data[0], data[1]);
     }
 }
