ddb98a3db8ac87da876bba1410cc98140d81dfa4
Anopob <anopob@hotmail.com>
2022-01-28 21:13:19 -0500

    Update MainMenu scene

---
+++ GGJLondon/Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    private SceneCalculator _sceneCalculator;
+
+    private void Start()
+    {
+        _sceneCalculator = FindObjectOfType<SceneCalculator>();
+    }
+
+    public void StartNewGame()
+    {
+        _sceneCalculator.StartNewGame();
+    }
+
+    public void GoToLevelSelect()
+    {
+        _sceneCalculator.GoToLevelSelect();
+    }
+}
