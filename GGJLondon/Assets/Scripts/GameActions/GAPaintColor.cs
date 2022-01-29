cd0e36111f57fe567a5f355092be4f59f13d9328
Neil Warnock <Neil@ecere.com>
2022-01-29 01:48:53 -0500

    New paint action, paints a single tile a specified colour

---
+++ GGJLondon/Assets/Scripts/GameActions/GAPaintColor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class GAPaintColor : GameAction
+{
+    protected Color _oldColor;
+    protected Color _paintColor;
+    
+    public GAPaintColor(int x, int y, Board board, Color colour) : base(x, y, board)
+    {
+        _paintColor = colour;
+    }
+
+    public override bool Redo()
+    {
+        
+        _oldColor = _board.GetTile(_x, _y).colour;
+        _board.ColourTile(_x, _y, _paintColor);
+        return (_oldColor != _paintColor);
+    }
+
+    public override void Undo()
+    {
+        _board.GetTile(_x, _y).SetColour(_oldColor);
+    }
+}
