3e520a99528e0cddc794f692fc3f814c9be2de0f
Anopob <anopob@hotmail.com>
2022-01-29 02:44:48 -0500

    Add instructions display

--- GGJLondon/Assets/Scripts/Levels/LevelInitializer.cs
+++ GGJLondon/Assets/Scripts/Levels/LevelInitializer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TMPro;
 using UnityEngine;
 
 namespace Assets.Scripts.Levels
@@ -13,15 +14,23 @@ namespace Assets.Scripts.Levels
         [SerializeField]
         private Board _boardPrefab;
 
+        private TextMeshProUGUI[] _instructions = new TextMeshProUGUI[4];
+
         private void Awake()
         {
             _sceneCalculator = FindObjectOfType<SceneCalculator>();
+            _instructions = GetComponentsInChildren<TextMeshProUGUI>();
         }
 
         private void Start()
         {
             LevelManager manager = (LevelManager)gameObject.AddComponent(_sceneCalculator.GetLevelScript());
             manager.SetupBoards(_boardPrefab);
+
+            _instructions[0].text += manager.LeftBoardLeftClickInstructions;
+            _instructions[1].text += manager.LeftBoardRightClickInstructions;
+            _instructions[2].text += manager.RightBoardLeftClickIntructions;
+            _instructions[3].text += manager.RightBoardRightClickIntructions;
         }
     }
 }
